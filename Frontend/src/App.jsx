4bd92e58f9de272034cecb9b7ac773e7f58691c0
Dhoundiyal-Anshul <dhoundiyalbest2010@gmail.com>
2024-12-12 13:56:25 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import WebcamCapture from "./Components/WebcamComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <WebcamCapture />
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Components/WebcamComponent.jsx
@@ -0,0 +1,51 @@
+import Webcam from "react-webcam";
+import Tesseract from "tesseract.js";
+import { isBrowser } from "react-device-detect";
+import { React, useRef, useState } from "react";
+let videoConstraints;
+if (isBrowser) {
+  videoConstraints = {
+    facingMode: { exact: "user" },
+  };
+} else {
+  videoConstraints = {
+    facingMode: { exact: "environment" },
+  };
+}
+const WebcamCapture = () => {
+  const webcamRef = useRef(null);
+  const [text, settext] = useState(" ");
+
+  const screenshot = () => {
+    if (webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      console.log(imageSrc);
+      const imgtotext = async () => {
+        const {
+          data: { text },
+        } = await Tesseract.recognize(imageSrc, "eng");
+        console.log(text);
+        settext(text);
+      };
+      imgtotext();
+    }
+  };
+  return (
+    <div>
+      <Webcam
+        ref={webcamRef}
+        audio={false}
+        disablePictureInPicture={false}
+        screenshotFormat="image/jpeg"
+        screenshotQuality="1"
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={screenshot}>Capture</button>
+      <div>
+        <h3>Extracted Text:</h3>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+export default WebcamCapture;
